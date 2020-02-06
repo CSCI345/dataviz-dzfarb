3212378b260b78cc5161472403e681cec35b8472
Mahlon Dizzy Farbanish <47366503+dzfarb@users.noreply.github.com>
2020-02-06 16:17:17 -0500

    first push

--- books.py
+++ books.py
@@ -1,24 +1,11 @@
-import matplotlib
+import matplotlib.pyplot as plt; plt.rcdefaults()
 from collections import defaultdict
 import sys
 import string
 import os
 import numpy as np
 
-stopwords = set(["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you",
-                         "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself",
-                         "she", "her", "hers", "herself", "it", "its", "itself", "they", "them",
-                         "their", "theirs", "themselves", "what", "which", "who", "whom", "this",
-                         "that", "these", "those", "am", "is", "are", "was", "were", "be", "been",
-                         "being", "have", "has", "had", "having", "do", "does", "did", "doing",
-                         "a", "an", "the", "and", "but", "if", "or", "because", "as", "until",
-                         "while", "of", "at", "by", "for", "with", "about", "against", "between",
-                         "into", "through", "during", "before", "after", "above", "below", "to",
-                         "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-                         "further", "then", "once", "here", "there", "when", "where", "why",
-                         "how", "all", "any", "both", "each", "few", "more", "most", "other",
-                         "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-                         "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", ""])
+stopwords = set()
 
 punctuation = set(string.punctuation)
 dir = "booksdir"
@@ -33,14 +20,6 @@ def parsefile(filename):
     file.close()
     return lines
 
-def removeStops(words):
-    clean_words = []
-    for w in words:
-        if w in self.stopword_set:
-            continue
-        clean_words.append(w)
-    return clean_words
-
 def createMap(lines):
     words = defaultdict(int)
     for l in lines:
@@ -48,12 +27,11 @@ def createMap(lines):
         l = ''.join(ch for ch in l if ch not in punctuation)
         l = l.lower().split(" ")
         for w in l:
-            if w not in stopwords:
-                words[w] += 1
+            words[w] += 1
     return words
 
 if __name__ == "__main__":
-    testword = "while"
+    inputword = "the"
 
     directory = os.fsencode(dir)
 
@@ -77,11 +55,12 @@ if __name__ == "__main__":
 
     performance = []
     for title in objects:
-        
+        performance.append(books[title][inputword])
 
+    #switch to plotly
     plt.bar(y_pos, performance, align='center', alpha=0.5)
     plt.xticks(y_pos, objects)
-    plt.ylabel('Usage')
-    plt.title('Programming language usage')
+    plt.ylabel('frequency')
+    plt.title('input word frequency in books')
 
     plt.show()
